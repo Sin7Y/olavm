26de9aa562ece2e2a23673d4532b2c4599d57cd8
Spartucus <spartucus@users.noreply.github.com>
2022-10-26 14:23:01 +0800

    add overflow add unit test, refactor code

--- prover/src/columns.rs
+++ prover/src/columns.rs
@@ -1,4 +1,4 @@
-pub const REG_LEN: usize = 16;
+pub (crate) const REG_LEN: usize = 16;
 
 // The trace for starky should be like:
 // ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬────────┬────────┐
@@ -8,41 +8,41 @@ pub const REG_LEN: usize = 16;
 // └───────┴───────┴───────┴───────┴───────┴───────┴───────┴────────┴────────┘
 
 // columns bumber of trace.
-pub const INST_COL: usize = 0;
-pub const CLK_COL: usize = INST_COL + 1;
-pub const PC_COL: usize = CLK_COL + 1;
-pub const FLAG_COL: usize = PC_COL + 1;
+pub (crate) const COL_INST: usize = 0;
+pub (crate) const COL_CLK: usize = COL_INST + 1;
+pub (crate) const COL_PC: usize = COL_CLK + 1;
+pub (crate) const COL_FLAG: usize = COL_PC + 1;
 
 // start column of register array.
-pub const REG_COL: usize = FLAG_COL + 1;
+pub (crate) const COL_REG: usize = COL_FLAG + 1;
 
 // For arithmetic operation, we need `NUM_ARITH_COLS` columns for stark trace.
-pub const NUM_ARITH_COLS: usize = REG_COL + 3;
+pub (crate) const NUM_ARITH_COLS: usize = COL_REG + 3;
 
 // ADD register columns.
 // reg_0 for output.
-pub const ADD_OUTPUT_COL: usize = REG_COL;
+pub (crate) const COL_ADD_OUTPUT: usize = COL_REG;
 // reg_1 for input0.
-pub const ADD_INPUT0_COL: usize = ADD_OUTPUT_COL + 1;
+pub (crate) const COL_ADD_INPUT0: usize = COL_ADD_OUTPUT + 1;
 // reg_2 for input1.
-pub const ADD_INPUT1_COL: usize = ADD_INPUT0_COL + 1;
+pub (crate) const COL_ADD_INPUT: usize = COL_ADD_INPUT0 + 1;
 
 // MUL register columns.
 // reg_0 for output.
-pub const MUL_OUTPUT_COL: usize = REG_COL;
+pub (crate) const COL_MUL_OUTPUT: usize = COL_REG;
 // reg_1 for input0.
-pub const MUL_INPUT0_COL: usize = MUL_OUTPUT_COL + 1;
+pub (crate) const COL_MUL_INPUT0: usize = COL_MUL_OUTPUT + 1;
 // reg_2 for input1.
-pub const MUL_INPUT1_COL: usize = MUL_INPUT0_COL + 1;
+pub (crate) const COL_MUL_INPUT1: usize = COL_MUL_INPUT0 + 1;
 
 // usize type of instruction.
-pub const ADD_ID:    usize = 1;
-pub const MUL_ID:    usize = 2;
-pub const EQ_ID:     usize = 3;
-pub const MOV_ID:    usize = 4;
-pub const JMP_ID:    usize = 5;
-pub const CJMP_ID:   usize = 6;
-pub const CALL_ID:   usize = 7;
-pub const RET_ID:    usize = 8;
-pub const MLOAD_ID:  usize = 9;
-pub const MSTORE_ID: usize = 10;
+pub (crate) const ADD_ID:    usize = 1;
+pub (crate) const MUL_ID:    usize = 2;
+pub (crate) const EQ_ID:     usize = 3;
+pub (crate) const MOV_ID:    usize = 4;
+pub (crate) const JMP_ID:    usize = 5;
+pub (crate) const CJMP_ID:   usize = 6;
+pub (crate) const CALL_ID:   usize = 7;
+pub (crate) const RET_ID:    usize = 8;
+pub (crate) const MLOAD_ID:  usize = 9;
+pub (crate) const MSTORE_ID: usize = 10;
