5bb2cde6bb30754797834c2762e020683996173e
Pierre Hong <hongyuanyang2019@163.com>
2022-11-24 14:38:49 +0800

    add hash

--- vm-core/src/program/mod.rs
+++ vm-core/src/program/mod.rs
@@ -9,6 +9,7 @@ pub const REGISTER_NUM: usize = 16;
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Program {
     pub instructions: Vec<String>,
+    // pub builtin: Vec<(String)>,
     pub trace: Trace,
 }
 
