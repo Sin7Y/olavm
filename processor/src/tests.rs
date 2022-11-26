0d70a87f4a6dc38bcc0823409da9881f60a26a18
Pierre Hong <hongyuanyang2019@163.com>
2022-11-26 15:51:48 +0800

    add excel

--- processor/src/tests.rs
+++ processor/src/tests.rs
@@ -265,3 +265,39 @@ fn prophet_range_check_test() {
     let mut file = File::create("fibo_trace.txt").unwrap();
     file.write_all(trace_json_format.as_ref()).unwrap();
 }
+
+use std::os::raw::c_char;
+use xlsxwriter::{Format, FormatAlignment, FormatColor, FormatUnderline, Workbook};
+
+#[test]
+fn export_excel_test() {
+    let workbook = Workbook::new("simple1.xlsx");
+
+    let mut sheet1 = workbook.add_worksheet(None).unwrap();
+    sheet1.write_string(
+        0,
+        0,
+        "Red text",
+        None
+    ).unwrap();
+    sheet1.write_number(0, 1, 20., None).unwrap();
+    sheet1.write_formula_num(1, 0, "=10+B1", None, 30.).unwrap();
+    sheet1.write_url(
+        1,
+        1,
+        "https://github.com/informationsea/xlsxwriter-rs",
+        None
+    ).unwrap();
+    sheet1.merge_range(
+        2,
+        0,
+        3,
+        2,
+        "Hello, world",
+        None
+    ).unwrap();
+
+    sheet1.set_selection(1, 0, 1, 2);
+    sheet1.set_tab_color(FormatColor::Cyan);
+    workbook.close();
+}
