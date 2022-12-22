290d3e94cfb93afce65b0c0a23c5af85777f326a
Pierre Hong <hongyuanyang2019@163.com>
2022-12-22 11:35:39 +0800

    fix memory bug

--- circuits/src/cpu/ret.rs
+++ circuits/src/cpu/ret.rs
@@ -1,6 +1,6 @@
 use crate::columns::*;
-use plonky2::field::packed::PackedField;
 use crate::constraint_consumer::ConstraintConsumer;
+use plonky2::field::packed::PackedField;
 #[allow(dead_code)]
 pub(crate) fn eval_packed_generic<P: PackedField>(
     lv: &[P; NUM_CPU_COLS],
