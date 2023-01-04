9d63130053db135c8a15f9020740bbccfaa2053d
Spartucus <spartucus@users.noreply.github.com>
2023-01-04 19:53:44 +0800

    fix call/ret cs

--- circuits/src/cpu/ret.rs
+++ circuits/src/cpu/ret.rs
@@ -8,13 +8,13 @@ pub(crate) fn eval_packed_generic<P: PackedField>(
     nv: &[P; NUM_CPU_COLS],
     yield_constr: &mut ConstraintConsumer<P>,
 ) {
-    // op0 + 1 - fp' = 0
+    // op0 + 1 - fp = 0
     // dst - pc' = 0
-    // aux0 - fp' + 2 = 0
-    let n_fp = nv[COL_REGS.end - 1];
-    let op0_cs = lv[COL_OP0] + P::ONES - n_fp;
+    // aux0 - fp + 2 = 0
+    let fp = lv[COL_REGS.end - 1];
+    let op0_cs = lv[COL_OP0] + P::ONES - fp;
     let dst_cs = lv[COL_DST] - nv[COL_PC];
-    let aux0_cs = lv[COL_AUX0] + P::ONES + P::ONES - n_fp;
+    let aux0_cs = lv[COL_AUX0] + P::ONES + P::ONES - fp;
 
     yield_constr.constraint(lv[COL_S_RET] * (op0_cs + dst_cs + aux0_cs));
 }
