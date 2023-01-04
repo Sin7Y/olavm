9b14afe1c5b57504abd50c9ee8aa34731cc3c062
Spartucus <spartucus@users.noreply.github.com>
2023-01-04 17:39:35 +0800

    1. add aux1 to gen_cpu_trace 2. fix call/ret cpu constraint

--- circuits/src/cpu/call.rs
+++ circuits/src/cpu/call.rs
@@ -5,13 +5,17 @@ use plonky2::field::packed::PackedField;
 #[allow(dead_code)]
 pub(crate) fn eval_packed_generic<P: PackedField>(
     lv: &[P; NUM_CPU_COLS],
-    _nv: &[P; NUM_CPU_COLS],
+    nv: &[P; NUM_CPU_COLS],
     yield_constr: &mut ConstraintConsumer<P>,
 ) {
-    // op0 + 1 - fp = 0
-    // op1_imm * (op1 - pc - 2) + (1 - op1_imm) * (op1 - pc - 1) = 0
-    let op0_cs = lv[COL_OP0] + P::ONES - lv[COL_REGS.end - 1];
-    let op1_cs = lv[COL_OP1_IMM] * (lv[COL_OP1] - lv[COL_PC] - P::ONES - P::ONES)
-        + (P::ONES - lv[COL_OP1_IMM]) * (lv[COL_OP1] - lv[COL_PC] - P::ONES);
-    yield_constr.constraint(lv[COL_S_CALL] * (op0_cs + op1_cs));
+    // op0 + 1 - fp' = 0
+    // op1_imm * (dst - pc - 2) + (1 - op1_imm) * (dst - pc - 1) = 0
+    // aux0 - fp' + 2 = 0
+    let n_fp = nv[COL_REGS.end - 1];
+    let op0_cs = lv[COL_OP0] + P::ONES - n_fp;
+    let op1_cs = lv[COL_OP1_IMM] * (lv[COL_DST] - lv[COL_PC] - P::ONES - P::ONES)
+        + (P::ONES - lv[COL_OP1_IMM]) * (lv[COL_DST] - lv[COL_PC] - P::ONES);
+    let aux0_cs = lv[COL_AUX0] - n_fp + P::ONES + P::ONES;
+
+    yield_constr.constraint(lv[COL_S_CALL] * (op0_cs + op1_cs + aux0_cs));
 }
--- circuits/src/cpu/ret.rs
+++ circuits/src/cpu/ret.rs
@@ -1,21 +1,20 @@
 use crate::columns::*;
 use crate::constraint_consumer::ConstraintConsumer;
 use plonky2::field::packed::PackedField;
+
 #[allow(dead_code)]
 pub(crate) fn eval_packed_generic<P: PackedField>(
     lv: &[P; NUM_CPU_COLS],
-    _nv: &[P; NUM_CPU_COLS],
+    nv: &[P; NUM_CPU_COLS],
     yield_constr: &mut ConstraintConsumer<P>,
 ) {
-    // op0 + 1 - fp = 0
-    // op1 - pc = 0
-    // dst + 2 - fp = 0
-    // aux0 - fp = 0
-    yield_constr.constraint(
-        lv[COL_S_RET]
-            * ((lv[COL_OP0] + P::ONES - lv[COL_REGS.end - 1])
-                + (lv[COL_OP1] - lv[COL_PC])
-                + (lv[COL_DST] + P::ONES + P::ONES - lv[COL_REGS.end - 1])
-                + (lv[COL_AUX0] - lv[COL_REGS.end - 1])),
-    );
+    // op0 + 1 - fp' = 0
+    // dst - pc' = 0
+    // aux0 - fp' + 2 = 0
+    let n_fp = nv[COL_REGS.end - 1];
+    let op0_cs = lv[COL_OP0] + P::ONES - n_fp;
+    let dst_cs = lv[COL_DST] - nv[COL_PC];
+    let aux0_cs = lv[COL_AUX0] + P::ONES + P::ONES - n_fp;
+
+    yield_constr.constraint(lv[COL_S_RET] * (op0_cs + dst_cs + aux0_cs));
 }
