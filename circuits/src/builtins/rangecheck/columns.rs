4eb2de6f62dcb1b99b99f241517700066432b3f9
jiangxiaobai <15800375054@163.com>
2023-01-07 12:05:30 +0800

    fixed cs logic

--- circuits/src/builtins/rangecheck/columns.rs
+++ circuits/src/builtins/rangecheck/columns.rs
@@ -32,8 +32,9 @@ pub(crate) const LIMB_LO_PERMUTED: usize = LIMB_HI + 1;
 pub(crate) const LIMB_HI_PERMUTED: usize = LIMB_LO_PERMUTED + 1;
 
 pub(crate) const FIX_RANGE_CHECK_U16: usize = LIMB_HI_PERMUTED + 1;
-pub(crate) const FIX_RANGE_CHECK_U16_PERMUTED: usize = FIX_RANGE_CHECK_U16 + 1;
+pub(crate) const FIX_RANGE_CHECK_U16_PERMUTED_LO: usize = FIX_RANGE_CHECK_U16 + 1;
+pub(crate) const FIX_RANGE_CHECK_U16_PERMUTED_HI: usize = FIX_RANGE_CHECK_U16_PERMUTED_LO + 1;
 
-pub(crate) const COL_NUM_RC: usize = FIX_RANGE_CHECK_U16_PERMUTED + 1; //3
+pub(crate) const COL_NUM_RC: usize = FIX_RANGE_CHECK_U16_PERMUTED_HI + 1; //9
 
 pub(crate) const RANGE_CHECK_U16_SIZE: usize = 1 << 16; //4
