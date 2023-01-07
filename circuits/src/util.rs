74bb9534192c1a4d697b9ce28e1aafecf72cebc8
jiangxiaobai <15800375054@163.com>
2023-01-07 11:53:07 +0800

    fixed limbs computation error

--- circuits/src/util.rs
+++ circuits/src/util.rs
@@ -523,6 +523,8 @@ pub fn generate_builtins_cmp_trace<F: RichField>(
             row[cmp::OP0] = F::from_canonical_u64(c.op0.to_canonical_u64());
             row[cmp::OP1] = F::from_canonical_u64(c.op1.to_canonical_u64());
             row[cmp::DIFF] = F::from_canonical_u64(c.diff.to_canonical_u64());
+            row[cmp::DIFF_LIMB_LO] = F::from_canonical_u64(c.diff_limb_lo.to_canonical_u64());
+            row[cmp::DIFF_LIMB_HI] = F::from_canonical_u64(c.diff_limb_hi.to_canonical_u64());
 
             row
         })
