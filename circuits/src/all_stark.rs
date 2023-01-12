381dc764e7f52b541f06253d9e03b1647c723ba8
Spartucus <spartucus@users.noreply.github.com>
2023-01-12 20:15:22 +0800

    add parameter to all_stark

--- circuits/src/all_stark.rs
+++ circuits/src/all_stark.rs
@@ -403,7 +403,7 @@ mod tests {
 
         println!("vm trace: {:?}", program.trace);
 
-        let cpu_rows = generate_cpu_trace::<F>(&program.trace.exec, &vec![]);
+        let cpu_rows = generate_cpu_trace::<F>(&program.trace.exec, &program.trace.raw_binary_instructions);
 
         println!("cpu rows: {:?}", cpu_rows);
 
@@ -442,7 +442,7 @@ mod tests {
 
         println!("vm trace: {:?}", program.trace);
 
-        let cpu_rows = generate_cpu_trace::<F>(&program.trace.exec, &vec![]);
+        let cpu_rows = generate_cpu_trace::<F>(&program.trace.exec, &program.trace.raw_binary_instructions);
 
         println!("cpu rows: {:?}", cpu_rows);
         trace_rows_to_poly_values(cpu_rows)
