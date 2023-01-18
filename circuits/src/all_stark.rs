efc5de405b012a5fa95e56c7f4b28d18ca286891
peinlcy <peinlcy@gmail.com>
2023-01-18 10:30:41 +0800

    remove unused code

--- circuits/src/all_stark.rs
+++ circuits/src/all_stark.rs
@@ -334,7 +334,6 @@ mod tests {
     use log::debug;
     use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
     use plonky2::util::timing::TimingTree;
-    use std::time::{SystemTime, UNIX_EPOCH};
 
     #[allow(dead_code)]
     const D: usize = 2;
@@ -627,16 +626,6 @@ mod tests {
             &mut TimingTree::default(),
         )?;
         verify_proof(all_stark, proof, &config)
-    }
-
-    #[allow(unused)]
-    fn timestamp() -> i64 {
-        let start = SystemTime::now();
-        let since_the_epoch = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards");
-        since_the_epoch.as_secs() as i64 * 1000i64
-            + (since_the_epoch.subsec_nanos() as f64 / 1_000_000.0) as i64
     }
 
     #[test]
