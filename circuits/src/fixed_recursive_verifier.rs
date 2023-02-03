f7b8417670940c964d03967c17be3cb803d9285a
peinlcy <peinlcy@gmail.com>
2023-02-03 18:02:36 +0800

    add recursive_verifier

--- circuits/src/fixed_recursive_verifier.rs
+++ circuits/src/fixed_recursive_verifier.rs
@@ -1,25 +1,105 @@
-// use plonky2::plonk::circuit_data::CircuitData;
+use std::collections::BTreeMap;
 
+use plonky2::{
+    field::extension::Extendable,
+    hash::hash_types::RichField,
+    iop::target::{BoolTarget, Target},
+    plonk::{
+        circuit_data::{CircuitData, VerifierCircuitTarget},
+        config::GenericConfig,
+        proof::{Proof, ProofWithPublicInputsTarget},
+    },
+};
 
-/// The recursion threshold. We end a chain of recursive proofs once we reach this size.
+use crate::{
+    all_stark::NUM_TABLES,
+    recursive_verifier::{PlonkWrapperCirCuit, StarkWrapperCircuit},
+};
+
+/// The recursion threshold. We end a chain of recursive proofs once we reach
+/// this size.
 const THRESHOLD_DEGREE_BITS: usize = 13;
 
-// /// Contains all recursive circuits used in the system. For each STARK and each initial
-// /// `degree_bits`, this contains a chain of recursive circuits for shrinking that STARK from
-// /// `degree_bits` to a constant `THRESHOLD_DEGREE_BITS`. It also contains a special root circuit
-// /// for combining each STARK's shrunk wrapper proof into a single proof.
-// pub struct AllRecursiveCircuits<F, C, const D: usize>
-// where
-//     F: RichField + Extendable<D>,
-//     C: GenericConfig<D, F = F>,
-// {
-//     pub root: RootCircuitData<F, C, D>,
-// }
+// /// Contains all recursive circuits used in the system. For each STARK and
+// each initial /// `degree_bits`, this contains a chain of recursive circuits
+// for shrinking that STARK from /// `degree_bits` to a constant
+// `THRESHOLD_DEGREE_BITS`. It also contains a special root circuit /// for
+// combining each STARK's shrunk wrapper proof into a single proof.
+pub struct AllRecursiveCircuits<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    pub root: RootCircuitData<F, C, D>,
+    pub aggregation: AggregationCircuitData<F, C, D>,
+    pub block: BlockCircuitData<F, C, D>,
+    by_tables: [RecursiveCircuitsForTable<F, C, D>; NUM_TABLES],
+}
 
+/// Data for the EVM root circuit, which is used to combine each STARK's shrunk
+/// wrapper proof into a single proof.
 pub struct RootCircuitData<F, C, const D: usize>
 where
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
 {
     circuit: CircuitData<F, C, D>,
+    proof_with_pis: [ProofWithPublicInputsTarget<D>; NUM_TABLES],
+    /// For each table, various inner circuits may be used depending on the
+    /// initial table size. This target holds the index of the circuit
+    /// (within `final_circuits()`) that was used.
+    index_verifier_data: [Target; NUM_TABLES],
+    /// Public inputs used for cyclic verification. These aren't actually used
+    /// for EVM root proofs; the circuit has them just to match the
+    /// structure of aggregation proofs.
+    cyclic_vk: VerifierCircuitTarget,
+}
+
+/// Data for the aggregation circuit, which is used to compress two proofs into
+/// one. Each inner proof can be either an EVM root proof or another aggregation
+/// proof.
+pub struct AggregationCircuitData<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    circuit: CircuitData<F, C, D>,
+    lhs: AggregationChildTarget<D>,
+    rhs: AggregationChildTarget<D>,
+    cyclic_vk: VerifierCircuitTarget,
+}
+
+pub struct AggregationChildTarget<const D: usize> {
+    is_agg: BoolTarget,
+    agg_proof: ProofWithPublicInputsTarget<D>,
+    evm_proof: ProofWithPublicInputsTarget<D>,
+}
+
+pub struct BlockCircuitData<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    circuit: CircuitData<F, C, D>,
+    has_parent_block: BoolTarget,
+    parent_block_proof: ProofWithPublicInputsTarget<D>,
+    agg_root_proof: ProofWithPublicInputsTarget<D>,
+    cyclic_vk: VerifierCircuitTarget,
+}
+
+struct RecursiveCircuitsForTable<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    by_stark_size: BTreeMap<usize, RecursiveCircuitsForTableSize<F, C, D>>,
+}
+
+struct RecursiveCircuitsForTableSize<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    initial_wrapper: StarkWrapperCircuit<F, C, D>,
+    shrinking_wrappers: Vec<PlonkWrapperCirCuit<F, C, D>>,
 }
---
+++ circuits/src/recursive_verifier.rs
@@ -0,0 +1,55 @@
+use std::fmt::Debug;
+
+use plonky2::{
+    field::extension::Extendable,
+    hash::{hash_types::RichField, hashing::SPONGE_WIDTH},
+    iop::target::Target,
+    plonk::{
+        circuit_data::CircuitData,
+        config::GenericConfig,
+        proof::{ProofWithPublicInputs, ProofWithPublicInputsTarget},
+    },
+};
+
+use crate::{
+    all_stark::NUM_TABLES, permutation::GrandProductChallengeSet, proof::StarkProofTarget,
+};
+
+/// Table-wise recursive proofs of an `AllProof`.
+pub struct RecursiveAllProof<
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+> {
+    pub recursive_proofs: [ProofWithPublicInputs<F, C, D>; NUM_TABLES],
+}
+
+pub(crate) struct PublicInputs<T: Copy + Eq + PartialEq + Debug> {
+    pub(crate) trace_cap: Vec<Vec<T>>,
+    pub(crate) ctl_zs_last: Vec<T>,
+    pub(crate) ctl_challenges: GrandProductChallengeSet<T>,
+    pub(crate) challenger_state_before: [T; SPONGE_WIDTH],
+    pub(crate) challenger_state_after: [T; SPONGE_WIDTH],
+}
+
+pub(crate) struct StarkWrapperCircuit<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    pub(crate) circuit: CircuitData<F, C, D>,
+    pub(crate) stark_proof_target: StarkProofTarget<D>,
+    pub(crate) ctl_challenges_target: GrandProductChallengeSet<Target>,
+    pub(crate) init_challenger_state_target: [Target; SPONGE_WIDTH],
+    pub(crate) zero_target: Target,
+}
+
+/// Represents a circuit which recursively verifies a PLONK proof.
+pub(crate) struct PlonkWrapperCirCuit<F, C, const D: usize>
+where
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    pub(crate) circuit: CircuitData<F, C, D>,
+    pub(crate) proof_with_pis_target: ProofWithPublicInputsTarget<D>,
+}
