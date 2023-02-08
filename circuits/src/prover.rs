0c363bda2fd9a97e0964ed94bdc71b83f5dfb26f
peinlcy <peinlcy@gmail.com>
2023-02-08 21:04:15 +0800

    test

--- circuits/src/prover.rs
+++ circuits/src/prover.rs
@@ -275,37 +275,37 @@ where
     //         config,
     //     );
     // }
-    // let quotient_polys = timed!(
-    //     timing,
-    //     "compute quotient polys",
-    //     compute_quotient_polys::<F, <F as Packable>::Packing, C, S, D>(
-    //         stark,
-    //         trace_commitment,
-    //         &permutation_ctl_zs_commitment,
-    //         permutation_challenges.as_ref(),
-    //         ctl_data,
-    //         alphas,
-    //         degree_bits,
-    //         num_permutation_zs,
-    //         config,
-    //     )
-    // );
-    // let all_quotient_chunks = timed!(
-    //     timing,
-    //     "split quotient polys",
-    //     quotient_polys
-    //         .into_par_iter()
-    //         .flat_map(|mut quotient_poly| {
-    //             quotient_poly
-    //                 .trim_to_len(degree * stark.quotient_degree_factor())
-    //                 .expect(
-    //                     "Quotient has failed, the vanishing polynomial is not divisible by Z_H",
-    //                 );
-    //             // Split quotient into degree-n chunks.
-    //             quotient_poly.chunks(degree)
-    //         })
-    //         .collect()
-    // );
+    let quotient_polys = timed!(
+        timing,
+        "compute quotient polys",
+        compute_quotient_polys::<F, <F as Packable>::Packing, C, S, D>(
+            stark,
+            trace_commitment,
+            &permutation_ctl_zs_commitment,
+            permutation_challenges.as_ref(),
+            ctl_data,
+            alphas,
+            degree_bits,
+            num_permutation_zs,
+            config,
+        )
+    );
+    let all_quotient_chunks = timed!(
+        timing,
+        "split quotient polys",
+        quotient_polys
+            .into_par_iter()
+            .flat_map(|mut quotient_poly| {
+                quotient_poly
+                    .trim_to_len(degree * stark.quotient_degree_factor())
+                    .expect(
+                        "Quotient has failed, the vanishing polynomial is not divisible by Z_H",
+                    );
+                // Split quotient into degree-n chunks.
+                quotient_poly.chunks(degree)
+            })
+            .collect()
+    );
     // let quotient_commitment = timed!(
     //     timing,
     //     "compute quotient commitment",
