e967e83040e1a396789a8016c8b8dbea429d842d
Spartucus <spartucus@users.noreply.github.com>
2023-02-15 14:11:13 +0800

    Add blake3 Hasher & format code

--- plonky2/plonky2/src/fri/oracle.rs
+++ plonky2/plonky2/src/fri/oracle.rs
@@ -119,9 +119,10 @@ impl<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>
             .map(|p| {
                 assert_eq!(p.len(), degree, "Polynomial degrees inconsistent");
                 // p.lde(rate_bits)
-                //     .coset_fft_with_options(F::coset_shift(), Some(rate_bits), fft_root_table)
-                //     .values;
-                p.coset_fft_with_options(F::coset_shift(), None, None, 1 << rate_bits).values
+                //     .coset_fft_with_options(F::coset_shift(), Some(rate_bits),
+                // fft_root_table)     .values;
+                p.coset_fft_with_options(F::coset_shift(), None, None, 1 << rate_bits)
+                    .values
             })
             .chain(
                 (0..salt_size)
