3fa2d93a0b2f58592f036638cd87ad25c344df48
Softcloud88 <softcloud1988@gmail.com>
2023-02-20 18:06:19 +0800

    MOD: cmp stark table.

--- circuits/src/builtins/cmp/columns.rs
+++ circuits/src/builtins/cmp/columns.rs
@@ -1,20 +1,20 @@
 // 2022-12-19: written by xb
 
 /*
-+--------+-----+-----+-----+----------+--------------+
-| filter | op0 | op1 | gte | abs_diff | abs_diff_inv |
-+--------+-----+-----+-----+----------+--------------+
-|    1   |  5  |  3  |  1  |     2    |     (1/2)    |
-+--------+-----+-----+-----+----------+--------------+
-|    1   |  2  |  7  |  0  |     5    |     (1/5)    |
-+--------+-----+-----+-----+----------+--------------+
-|    1   |  6  |  6  |  1  |     0    |       0      |
-+--------+-----+-----+-----+----------+--------------+
++-----+-----+-----+----------+--------------+-------------------+
+| op0 | op1 | gte | abs_diff | abs_diff_inv | filter_looking_rc |
++-----+-----+-----+----------+--------------+-------------------+
+|  5  |  3  |  1  |     2    |     (1/2)    |         1         |
++-----+-----+-----+----------+--------------+-------------------+
+|  2  |  7  |  0  |     5    |     (1/5)    |         1         |
++-----+-----+-----+----------+--------------+-------------------+
+|  6  |  6  |  1  |     0    |       0      |         1         |
++-----+-----+-----+----------+--------------+-------------------+
 */
-pub(crate) const COL_CMP_FILTER: usize = 0;
-pub(crate) const COL_CMP_OP0: usize = COL_CMP_FILTER + 1;
+pub(crate) const COL_CMP_OP0: usize = 0;
 pub(crate) const COL_CMP_OP1: usize = COL_CMP_OP0 + 1;
 pub(crate) const COL_CMP_GTE: usize = COL_CMP_OP1 + 1;
 pub(crate) const COL_CMP_ABS_DIFF: usize = COL_CMP_GTE + 1;
 pub(crate) const COL_CMP_ABS_DIFF_INV: usize = COL_CMP_ABS_DIFF + 1;
-pub(crate) const COL_NUM_CMP: usize = COL_CMP_ABS_DIFF_INV + 1;
+pub(crate) const COL_CMP_FILTER_LOOKING_RC: usize = COL_CMP_ABS_DIFF_INV + 1;
+pub(crate) const COL_NUM_CMP: usize = COL_CMP_FILTER_LOOKING_RC + 1;
