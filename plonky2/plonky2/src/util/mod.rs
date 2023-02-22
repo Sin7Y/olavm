f3537a23d6ec1887fb4aa6b6dd67f68414e9dd7b
peinlcy <peinlcy@gmail.com>
2023-02-22 14:20:48 +0800

    add transpose parallel

--- plonky2/plonky2/src/util/mod.rs
+++ plonky2/plonky2/src/util/mod.rs
@@ -1,3 +1,4 @@
+use maybe_rayon::current_num_threads;
 use plonky2_field::polynomial::PolynomialValues;
 use plonky2_field::types::Field;
 
@@ -8,12 +9,48 @@ pub mod serialization;
 pub mod strided_view;
 pub mod timing;
 
+const MIN_BATCH_SIZE: usize = 128;
+
 pub(crate) fn transpose_poly_values<F: Field>(polys: Vec<PolynomialValues<F>>) -> Vec<Vec<F>> {
     let poly_values = polys.into_iter().map(|p| p.values).collect::<Vec<_>>();
     transpose(&poly_values)
 }
 
-// l * w => w * l
+#[cfg(feature = "parallel")]
+pub fn transpose_par<F: Field>(matrix: &[Vec<F>]) -> Vec<Vec<F>> {
+    use maybe_rayon::{MaybeParChunksMut, IndexedParallelIterator, ParallelIterator};
+
+    let l = matrix.len();
+    let w = matrix[0].len();
+
+    if w > l && w.is_power_of_two() {
+        let batch_size = w / current_num_threads().next_power_of_two();
+        if batch_size < MIN_BATCH_SIZE {
+            return transpose(matrix);
+        }
+        let mut transposed = vec![vec![]; w];
+        for i in 0..w {
+            transposed[i].reserve_exact(l);
+            unsafe {
+                transposed[i].set_len(l);
+            }
+        }
+        transposed.par_chunks_mut(batch_size).enumerate().for_each(|(i, batch)| {
+            let batch_offset = i * batch_size;
+            for (k, row_buf) in batch.iter_mut().enumerate() {
+                let j = k + batch_offset;
+                for i in 0..l {
+                    (*row_buf)[i] = matrix[i][j];
+                }
+            }
+        });
+        return transposed;
+    }
+
+    transpose(matrix)
+    
+}
+
 pub fn transpose<F: Field>(matrix: &[Vec<F>]) -> Vec<Vec<F>> {
     let l = matrix.len();
     let w = matrix[0].len();
