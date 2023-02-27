59a7c081eee3238362239d35a7ffb003b75e8cb7
Pierre Hong <hongyuanyang2019@163.com>
2023-02-27 15:27:38 +0800

    modify test use public encode file

--- executor/src/tests.rs
+++ executor/src/tests.rs
@@ -2,7 +2,7 @@ use crate::Process;
 use core::program::Program;
 use log::{debug, LevelFilter};
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::time::Instant;
 
 #[test]
@@ -25,37 +25,12 @@ fn memory_test() {
     // mload r0 [r5]
     // add r8 r8 -4
     // end
-    let program_src = "0x6000080400000000
-                             0x4
-                             0x4000008040000000
-                             0x64
-                             0x4210000001000000
-                             0xfffffffefffffffe
-                             0x4000008040000000
-                             0x1
-                             0x4210000001000000
-                             0xfffffffeffffffff
-                             0x4000008040000000
-                             0x2
-                             0x4210000001000000
-                             0xffffffff00000000
-                             0x4010008002000000
-                             0xfffffffefffffffe
-                             0x4010001002000000
-                             0xfffffffeffffffff
-                             0x4010000802000000
-                             0xffffffff00000000
-                             0x0200208400000000
-                             0x0200108200000000
-                             0x0202000001000000
-                             0x0002000802000000
-                             0x6000080400000000
-                             0xfffffffefffffffd
-                             0x0000000000800000";
+
     let _ = env_logger::builder()
         .filter_level(LevelFilter::Debug)
         .try_init();
-    let instructions = program_src.split('\n');
+    let file = File::open("../assembler/testdata/memory.bin").unwrap();
+    let mut instructions = BufReader::new(file).lines();
     let mut program: Program = Program {
         instructions: Vec::new(),
         trace: Default::default(),
@@ -63,7 +38,7 @@ fn memory_test() {
     debug!("instructions:{:?}", program.instructions);
 
     for inst in instructions.into_iter() {
-        program.instructions.push(inst.clone().parse().unwrap());
+        program.instructions.push(inst.unwrap());
     }
 
     let mut process = Process::new();
@@ -85,18 +60,9 @@ fn range_check_test() {
     //mul r4 r3 r2
     //range_check r4
     //end
-    let program_src = "0x4000000840000000
-        0x8
-        0x4000001040000000
-        0x2
-        0x4000002040000000
-        0x3
-        0x0020204400000000
-        0x0100408200000000
-        0x0001000000400000
-        0x0000000000800000";
 
-    let instructions = program_src.split('\n');
+    let file = File::open("../assembler/testdata/range_check.bin").unwrap();
+    let mut instructions = BufReader::new(file).lines();
     let mut program: Program = Program {
         instructions: Vec::new(),
         trace: Default::default(),
@@ -104,7 +70,7 @@ fn range_check_test() {
     debug!("instructions:{:?}", program.instructions);
 
     for inst in instructions.into_iter() {
-        program.instructions.push(inst.clone().parse().unwrap());
+        program.instructions.push(inst.unwrap());
     }
 
     let mut process = Process::new();
@@ -130,22 +96,9 @@ fn bitwise_test() {
     //or r3 r2 r3
     //and r4 r4 r3
     //end
-    let program_src = "0x4000000840000000
-                             0x8
-                             0x4000001040000000
-                             0x2
-                             0x4000002040000000
-                             0x3
-                             0x0020204400000000
-                             0x0100408200000000
-                             0x0200810000200000
-                             0x0041020000100000
-                             0x0400440000080000
-                             0x0080804000100000
-                             0x0200808000200000
-                             0x0000000000800000";
 
-    let instructions = program_src.split('\n');
+    let file = File::open("../assembler/testdata/bitwise.bin").unwrap();
+    let mut instructions = BufReader::new(file).lines();
     let mut program: Program = Program {
         instructions: Vec::new(),
         trace: Default::default(),
@@ -153,7 +106,7 @@ fn bitwise_test() {
     debug!("instructions:{:?}", program.instructions);
 
     for inst in instructions.into_iter() {
-        program.instructions.push(inst.clone().parse().unwrap());
+        program.instructions.push(inst.unwrap());
     }
 
     let mut process = Process::new();
@@ -168,32 +121,30 @@ fn bitwise_test() {
 
 #[test]
 fn comparison_test() {
-    //mov r0 8
-    //mov r1 2
-    //mov r2 3
-    //add r3 r0 r1
-    //mul r4 r3 r2
-    //gte r6 r4 r3
-    //cjmp r6 11
-    //add r3 r0 r2
-    //mul r4 r3 r0
-    //end
-    let program_src = "0x4000000840000000
-                             0x8
-                             0x4000001040000000
-                             0x2
-                             0x4000002040000000
-                             0x3
-                             0x0020204400000000
-                             0x0100408200000000
-                             0x0200820000010000
-                             0x4800000010000000
-                             0xb
-                             0x0020404400000000
-                             0x0100108200000000
-                             0x0000000000800000";
+    //"main:
+    //  .LBL0_0:
+    //    add r8 r8 4
+    //    mstore [r8,-2] r8
+    //    mov r1 1
+    //    call le
+    //    add r8 r8 -4
+    //    end
+    //  le:
+    //  .LBL1_0:
+    //    mov r0 r1
+    //    mov r7 1
+    //    gte r0 r7 r0
+    //    cjmp r0 .LBL1_1
+    //    jmp .LBL1_2
+    //  .LBL1_1:
+    //    mov r0 2
+    //    ret
+    //  .LBL1_2:
+    //    mov r0 3
+    //    ret"
 
-    let instructions = program_src.split('\n');
+    let file = File::open("../assembler/testdata/comparison.bin").unwrap();
+    let mut instructions = BufReader::new(file).lines();
     let mut program: Program = Program {
         instructions: Vec::new(),
         trace: Default::default(),
@@ -201,7 +152,7 @@ fn comparison_test() {
     debug!("instructions:{:?}", program.instructions);
 
     for inst in instructions.into_iter() {
-        program.instructions.push(inst.clone().parse().unwrap());
+        program.instructions.push(inst.unwrap());
     }
 
     let mut process = Process::new();
@@ -247,61 +198,10 @@ fn call_test() {
     //   mload r0 [r8,-5]
     //   add r8 r8 -5
     //   ret
-    let program_src = "0x6000080400000000
-                             0x5
-                             0x6010000001000000
-                             0xfffffffeffffffff
-                             0x4000000840000000
-                             0xa
-                             0x4030000001000000
-                             0xfffffffefffffffc
-                             0x4000000840000000
-                             0x14
-                             0x4030000001000000
-                             0xfffffffefffffffd
-                             0x4000000840000000
-                             0x64
-                             0x4030000001000000
-                             0xfffffffefffffffe
-                             0x4010001002000000
-                             0xfffffffefffffffc
-                             0x4010002002000000
-                             0xfffffffefffffffd
-                             0x4000000008000000
-                             0x1d
-                             0x4030000001000000
-                             0xfffffffefffffffe
-                             0x4010000802000000
-                             0xfffffffefffffffe
-                             0x6000080400000000
-                             0xfffffffefffffffc
-                             0x0000000000800000
-                             0x6000080400000000
-                             0x5
-                             0x4050000001000000
-                             0xfffffffefffffffe
-                             0x4090000001000000
-                             0xfffffffefffffffd
-                             0x4000001040000000
-                             0xc8
-                             0x4050000001000000
-                             0xfffffffefffffffc
-                             0x4010001002000000
-                             0xfffffffefffffffe
-                             0x4010002002000000
-                             0xfffffffefffffffd
-                             0x0040400c00000000
-                             0x4030000001000000
-                             0xfffffffefffffffc
-                             0x4010000802000000
-                             0xfffffffefffffffc
-                             0x6000080400000000
-                             0xfffffffefffffffc
-                             0x0000000004000000";
-    let _ = env_logger::builder()
-        .filter_level(LevelFilter::Debug)
-        .try_init();
-    let instructions = program_src.split('\n');
+
+    let file = File::open("../assembler/testdata/call.bin").unwrap();
+    let mut instructions = BufReader::new(file).lines();
+
     let mut program: Program = Program {
         instructions: Vec::new(),
         trace: Default::default(),
@@ -309,7 +209,7 @@ fn call_test() {
     debug!("instructions:{:?}", program.instructions);
 
     for inst in instructions.into_iter() {
-        program.instructions.push(inst.clone().parse().unwrap());
+        program.instructions.push(inst.unwrap());
     }
 
     let mut process = Process::new();
@@ -371,89 +271,18 @@ fn fibo_use_loop_decode() {
     //    mload r0 [r8,-4]
     //    add r8 r8 -5
     //   ret
-    let program_src = "0x6000080400000000
-                             0x4
-                             0x2010000001000000
-                             0xfffffffeffffffff
-                             0x4000001040000000
-                             0xa
-                             0x4000000008000000
-                             0xb
-                             0x6000080400000000
-                             0xfffffffefffffffd
-                             0x0000000000800000
-                             0x6000080400000000
-                             0x5
-                             0x0000200840000000
-                             0x0030000001000000
-                             0xffffffff00000000
-                             0x4000000840000000
-                             0x0
-                             0x0030000001000000
-                             0xfffffffeffffffff
-                             0x4000000840000000
-                             0x1
-                             0x0030000001000000
-                             0xfffffffefffffffe
-                             0x4000000840000000
-                             0x1
-                             0x0030000001000000
-                             0xfffffffefffffffd
-                             0x4000000840000000
-                             0x2
-                             0x0030000001000000
-                             0xfffffffefffffffc
-                             0x4000000020000000
-                             0x22
-                             0x0010000802000000
-                             0xfffffffefffffffc
-                             0x0010001002000000
-                             0xffffffff00000000
-                             0x0040100800010000
-                             0x4020000010000000
-                             0x2b
-                             0x4000000020000000
-                             0x44
-                             0x0010001002000000
-                             0xfffffffeffffffff
-                             0x0010002002000000
-                             0xfffffffefffffffe
-                             0x0040400c00000000
-                             0x0030000001000000
-                             0xfffffffefffffffd
-                             0x0010000802000000
-                             0xfffffffefffffffe
-                             0x0030000001000000
-                             0xfffffffeffffffff
-                             0x0010000802000000
-                             0xfffffffefffffffd
-                             0x0030000001000000
-                             0xfffffffefffffffe
-                             0x4000000020000000
-                             0x3c
-                             0x0010001002000000
-                             0xfffffffefffffffc
-                             0x4040000c00000000
-                             0x1
-                             0x0030000001000000
-                             0xfffffffefffffffc
-                             0x4000000020000000
-                             0x22
-                             0x0010000802000000
-                             0xfffffffefffffffd
-                             0x6000080400000000
-                             0xfffffffefffffffc
-                             0x0000000004000000";
 
-    let instructions = program_src.split('\n');
+    let file = File::open("../assembler/testdata/fibo_loop.bin").unwrap();
+    let mut instructions = BufReader::new(file).lines();
+
     let mut program: Program = Program {
         instructions: Vec::new(),
         trace: Default::default(),
     };
     debug!("instructions:{:?}", program.instructions);
 
-    for inst in instructions.into_iter() {
-        program.instructions.push(inst.clone().parse().unwrap());
+    for inst in instructions {
+        program.instructions.push(inst.unwrap());
     }
 
     let mut process = Process::new();
@@ -465,13 +294,16 @@ fn fibo_use_loop_decode() {
         exec_time.as_secs(),
         program.trace.exec.len()
     );
-    let file = File::create("fibo_loop.txt").unwrap();
+    let file = File::create("fib_loop.txt").unwrap();
 
     serde_json::to_writer(file, &program.trace).unwrap();
 }
 
 #[test]
 fn fibo_recursive() {
+    let _ = env_logger::builder()
+        .filter_level(LevelFilter::Debug)
+        .try_init();
     //  main:
     //  LBL0_0:
     //   add r8 r8 4
@@ -519,82 +351,9 @@ fn fibo_recursive() {
     //   mload r0 [r8,-6]
     //   add r8 r8 -9
     //   ret
-    let program_src = "0x6000080400000000
-                             0x4
-                             0x2010000001000000
-                             0xfffffffeffffffff
-                             0x4000001040000000
-                             0xa
-                             0x4000000008000000
-                             0xb
-                             0x6000080400000000
-                             0xfffffffefffffffd
-                             0x0000000000800000
-                             0x6000080400000000
-                             0x9
-                             0x2010000001000000
-                             0xfffffffeffffffff
-                             0x0000200840000000
-                             0x0030000001000000
-                             0xfffffffefffffffa
-                             0x0010000802000000
-                             0xfffffffefffffffa
-                             0x4020020100000000
-                             0x1
-                             0x4800000010000000
-                             0x1a
-                             0x4000000020000000
-                             0x1f
-                             0x4000000840000000
-                             0x1
-                             0x6000080400000000
-                             0xfffffffefffffff8
-                             0x0000000004000000
-                             0x0010000802000000
-                             0xfffffffefffffffa
-                             0x4020020100000000
-                             0x2
-                             0x4800000010000000
-                             0x27
-                             0x4000000020000000
-                             0x2c
-                             0x4000000840000000
-                             0x1
-                             0x6000080400000000
-                             0xfffffffefffffff8
-                             0x0000000004000000
-                             0x0010000802000000
-                             0xfffffffefffffffa
-                             0x4020001400000000
-                             0xffffffff00000000
-                             0x4000000008000000
-                             0xb
-                             0x0030000001000000
-                             0xfffffffefffffffe
-                             0x0010000802000000
-                             0xfffffffefffffffa
-                             0x4020000c00000000
-                             0xfffffffeffffffff
-                             0x0030000001000000
-                             0xfffffffefffffffc
-                             0x0010001002000000
-                             0xfffffffefffffffc
-                             0x4000000008000000
-                             0xb
-                             0x0010001002000000
-                             0xfffffffefffffffe
-                             0x0040100c00000000
-                             0x0030000001000000
-                             0xfffffffefffffffb
-                             0x0010000802000000
-                             0xfffffffefffffffb
-                             0x6000080400000000
-                             0xfffffffefffffff8
-                             0x0000000004000000";
-    let _ = env_logger::builder()
-        .filter_level(LevelFilter::Debug)
-        .try_init();
-    let instructions = program_src.split('\n');
+    let file = File::open("../assembler/testdata/fib_recursive.bin").unwrap();
+    let mut instructions = BufReader::new(file).lines();
+
     let mut program: Program = Program {
         instructions: Vec::new(),
         trace: Default::default(),
@@ -602,7 +361,7 @@ fn fibo_recursive() {
     debug!("instructions:{:?}", program.instructions);
 
     for inst in instructions.into_iter() {
-        program.instructions.push(inst.clone().parse().unwrap());
+        program.instructions.push(inst.unwrap());
     }
 
     let mut process = Process::new();
