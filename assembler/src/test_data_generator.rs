e4e702ce2080cf0d9f5efc259861642e19cd227b
Softcloud88 <softcloud1988@gmail.com>
2023-03-19 13:24:32 +0800

    ADD: tests.

--- assembler/src/test_data_generator.rs
+++ assembler/src/test_data_generator.rs
@@ -13,10 +13,43 @@ mod tests {
 
     #[test]
     fn generate_memory() {
+        generate_from_file("memory.json".to_string(), "memory.json".to_string());
+    }
+
+    #[test]
+    fn generate_call() {
+        generate_from_file("call.json".to_string(), "call.json".to_string());
+    }
+
+    #[test]
+    fn generate_range_check() {
         generate_from_file(
-            "memory.json".to_string(),
-            "memory.json".to_string(),
+            "range_check.json".to_string(),
+            "range_check.json".to_string(),
         );
+    }
+
+    #[test]
+    fn generate_bitwise() {
+        generate_from_file("bitwise.json".to_string(), "bitwise.json".to_string());
+    }
+
+    #[test]
+    fn generate_comparison() {
+        generate_from_file("comparison.json".to_string(), "comparison.json".to_string());
+    }
+
+    #[test]
+    fn generate_fibo_recursive() {
+        generate_from_file(
+            "fibo_recursive.json".to_string(),
+            "fibo_recursive.json".to_string(),
+        );
+    }
+
+    #[test]
+    fn generate_fibo_loop() {
+        generate_from_file("fibo_loop.json".to_string(), "fibo_loop.json".to_string());
     }
 
     fn generate_from_file(input_file_name: String, output_file_name: String) {
