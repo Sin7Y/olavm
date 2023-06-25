238dfd9fa576941048d318720b653b91987e3978
Softcloud88 <softcloud1988@gmail.com>
2023-06-25 15:10:53 +0800

    FIX: tests.

--- circuits/src/cpu/cpu_stark.rs
+++ circuits/src/cpu/cpu_stark.rs
@@ -679,10 +679,8 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for CpuStark<F, D
 #[cfg(test)]
 mod tests {
     use crate::{generation::cpu::generate_cpu_trace, test_utils::test_stark_with_asm_path};
-    use assembler::encoder::encode_asm_from_json_file;
     use core::trace::trace::{Step, Trace};
-    use std::io::BufRead;
-    use std::{collections::HashMap, path::PathBuf};
+    use std::path::PathBuf;
     use {
         super::*,
         core::program::Program,
@@ -801,49 +799,5 @@ mod tests {
                 Some(error_hook),
             );
         }
-    }
-
-    fn test_cpu_with_asm_file_name(file_name: String) {
-        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        path.push("../assembler/test_data/asm/");
-        path.push(file_name);
-        let program_path = path.display().to_string();
-
-        const D: usize = 2;
-        type C = PoseidonGoldilocksConfig;
-        type F = <C as GenericConfig<D>>::F;
-        type S = CpuStark<F, D>;
-        let stark = S::default();
-
-        let get_trace_rows = |trace: Trace| trace.exec;
-        let generate_trace = |rows: &[Step]| generate_cpu_trace(rows);
-        let eval_packed_generic =
-            |vars: StarkEvaluationVars<GoldilocksField, GoldilocksField, NUM_CPU_COLS>,
-             constraint_consumer: &mut ConstraintConsumer<GoldilocksField>| {
-                stark.eval_packed_generic(vars, constraint_consumer);
-            };
-        let error_hook = |i: usize,
-                          vars: StarkEvaluationVars<
-            GoldilocksField,
-            GoldilocksField,
-            NUM_CPU_COLS,
-        >| {
-            println!("constraint error in line {}", i);
-            let m = get_cpu_col_name_map();
-            println!("{:>32}\t{:>22}\t{:>22}", "name", "lv", "nv");
-            for col in m.keys() {
-                let name = m.get(col).unwrap();
-                let lv = vars.local_values[*col].0;
-                let nv = vars.next_values[*col].0;
-                println!("{:>32}\t{:>22}\t{:>22}", name, lv, nv);
-            }
-        };
-        test_stark_with_asm_path(
-            program_path.to_string(),
-            get_trace_rows,
-            generate_trace,
-            eval_packed_generic,
-            Some(error_hook),
-        );
     }
 }
