ab968d4e6d2cab08c1089d3784e9e2c59551b063
Softcloud88 <softcloud1988@gmail.com>
2023-09-04 19:30:23 +0800

    MOD: MemoryStark for tape and sccall.

--- circuits/src/memory/memory_stark.rs
+++ circuits/src/memory/memory_stark.rs
@@ -60,16 +60,28 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for MemoryStark<F
     {
         let lv = vars.local_values;
         let nv = vars.next_values;
+        // tx_idx not change or increase by one
+        yield_constr.constraint_transition(
+            (nv[COL_MEM_TX_IDX] - lv[COL_MEM_TX_IDX])
+                * (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX]),
+        );
+        // in same tx, env_idx not change or increase by one
+        yield_constr.constraint_transition(
+            (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX])
+                * (nv[COL_MEM_ENV_IDX] - lv[COL_MEM_ENV_IDX])
+                * (P::ONES - nv[COL_MEM_ENV_IDX] + lv[COL_MEM_ENV_IDX]),
+        );
 
         let p = P::ZEROS;
         let span = P::Scalar::from_canonical_u64(2_u64.pow(32).sub(1));
 
-        let op = lv[COL_MEM_OP];
         let is_rw = lv[COL_MEM_IS_RW];
-        let nv_is_rw = nv[COL_MEM_IS_RW];
         let region_prophet = lv[COL_MEM_REGION_PROPHET];
+        let nv_region_prophet = nv[COL_MEM_REGION_PROPHET];
         let region_heap = lv[COL_MEM_REGION_HEAP];
         let nv_region_heap = nv[COL_MEM_REGION_HEAP];
+        let region_stack = P::ONES - lv[COL_MEM_REGION_HEAP] - lv[COL_MEM_REGION_PROPHET];
+        let nv_region_stack = P::ONES - nv[COL_MEM_REGION_HEAP] - nv[COL_MEM_REGION_PROPHET];
         let is_write = lv[COL_MEM_IS_WRITE];
         let nv_is_write = nv[COL_MEM_IS_WRITE];
         let filter_looked_for_main = lv[COL_MEM_FILTER_LOOKED_FOR_MAIN];
@@ -93,67 +105,140 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for MemoryStark<F
         let op_mstore = P::Scalar::from_canonical_u64(OlaOpcode::MSTORE.binary_bit_mask());
         let op_call = P::Scalar::from_canonical_u64(OlaOpcode::CALL.binary_bit_mask());
         let op_ret = P::Scalar::from_canonical_u64(OlaOpcode::RET.binary_bit_mask());
-        // op is one of mload, mstore, call, ret or prophet write 0.
-        yield_constr
-            .constraint(op * (op - op_mload) * (op - op_mstore) * (op - op_call) * (op - op_ret));
-        // when op is 0, is_rw must be zero.
+        let op_tload = P::Scalar::from_canonical_u64(OlaOpcode::TLOAD.binary_bit_mask());
+        let op_tstore = P::Scalar::from_canonical_u64(OlaOpcode::TSTORE.binary_bit_mask());
+        let op_sc_call = P::Scalar::from_canonical_u64(OlaOpcode::SCCALL.binary_bit_mask());
+        let op_prophet = P::ZEROS;
+
+        // constraint opcode and selector matches, selector is binary and only one is
+        // selected.
+        yield_constr.constraint((lv[COL_MEM_OP] - op_mload) * lv[COL_MEM_S_MLOAD]);
+        yield_constr.constraint((lv[COL_MEM_OP] - op_mstore) * lv[COL_MEM_S_MSTORE]);
+        yield_constr.constraint((lv[COL_MEM_OP] - op_call) * lv[COL_MEM_S_CALL]);
+        yield_constr.constraint((lv[COL_MEM_OP] - op_ret) * lv[COL_MEM_S_RET]);
+        yield_constr.constraint((lv[COL_MEM_OP] - op_tload) * lv[COL_MEM_S_TLOAD]);
+        yield_constr.constraint((lv[COL_MEM_OP] - op_tstore) * lv[COL_MEM_S_TSTORE]);
+        yield_constr.constraint((lv[COL_MEM_OP] - op_sc_call) * lv[COL_MEM_S_SCCALL]);
+        yield_constr.constraint((lv[COL_MEM_OP] - op_prophet) * lv[COL_MEM_S_PROPHET]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_MLOAD]) * lv[COL_MEM_S_MLOAD]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_MSTORE]) * lv[COL_MEM_S_MSTORE]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_CALL]) * lv[COL_MEM_S_CALL]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_RET]) * lv[COL_MEM_S_RET]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_TLOAD]) * lv[COL_MEM_S_TLOAD]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_TSTORE]) * lv[COL_MEM_S_TSTORE]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_SCCALL]) * lv[COL_MEM_S_SCCALL]);
+        yield_constr.constraint((P::ONES - lv[COL_MEM_S_PROPHET]) * lv[COL_MEM_S_PROPHET]);
         yield_constr.constraint(
-            (op - op_mload) * (op - op_mstore) * (op - op_call) * (op - op_ret) * is_rw,
+            P::ONES
+                - lv[COL_MEM_S_MLOAD]
+                - lv[COL_MEM_S_MSTORE]
+                - lv[COL_MEM_S_CALL]
+                - lv[COL_MEM_S_RET]
+                - lv[COL_MEM_S_TLOAD]
+                - lv[COL_MEM_S_TSTORE]
+                - lv[COL_MEM_S_SCCALL]
+                - lv[COL_MEM_S_PROPHET],
         );
 
-        // constraint is_write and op. When write, op can be mstore, call and 0; When
-        // read, op can be mload, call, ret. call can both write and read, does
-        // not need a constraint rule.
-        yield_constr
-            .constraint((op - op_mload) * (op - op_call) * (op - op_ret) * (P::ONES - is_write));
-        yield_constr.constraint(op * (op - op_mstore) * (op - op_call) * is_write);
+        // constraint is_rw region
+        yield_constr.constraint(is_rw * (P::ONES - is_rw));
+        yield_constr.constraint(lv[COL_MEM_IS_RW] * lv[COL_MEM_S_PROPHET]);
+        yield_constr.constraint(
+            (P::ONES - lv[COL_MEM_IS_RW]) * (P::ONES - lv[COL_MEM_S_PROPHET] - lv[COL_MEM_S_MLOAD]),
+        );
+        // constraint is_write
+        yield_constr.constraint(
+            lv[COL_MEM_IS_WRITE]
+                * (P::ONES
+                    - lv[COL_MEM_S_MSTORE]
+                    - lv[COL_MEM_S_CALL]
+                    - lv[COL_MEM_S_TLOAD]
+                    - lv[COL_MEM_S_PROPHET]),
+        );
+        yield_constr.constraint(
+            (P::ONES - lv[COL_MEM_IS_WRITE])
+                * (P::ONES
+                    - lv[COL_MEM_S_MLOAD]
+                    - lv[COL_MEM_S_CALL]
+                    - lv[COL_MEM_S_RET]
+                    - lv[COL_MEM_S_TSTORE]
+                    - lv[COL_MEM_S_SCCALL]),
+        );
 
-        // when op is not 0, filter_looked_for_main need to be enabled.
-        yield_constr.constraint(op * (P::ONES - filter_looked_for_main));
+        // region is one of stack, heap and prophet
+        yield_constr.constraint(P::ONES - region_stack - region_heap - region_prophet);
+        yield_constr.constraint(region_stack * (P::ONES - region_stack));
+        yield_constr.constraint(region_heap * (P::ONES - region_heap));
+        yield_constr.constraint(region_prophet * (P::ONES - region_prophet));
+        // make sure region addr is right
+        yield_constr.constraint(region_prophet * (p - addr - diff_addr_cond));
+        yield_constr.constraint(region_heap * (p - span - addr - diff_addr_cond));
+
+        // if not prophet write, must be looked up.
+        yield_constr
+            .constraint((P::ONES - lv[COL_MEM_S_PROPHET]) * (P::ONES - filter_looked_for_main));
 
         // addr'-addr-diff_addr'= 0
         yield_constr.constraint_transition(
-            (nv_region_heap - region_heap - P::ONES) * (nv_addr - addr - nv_diff_addr),
-        );
-        // for rw_addr_unchanged
-        yield_constr.constraint_first_row(rw_addr_unchanged);
-        yield_constr.constraint_transition(
-            is_rw
-                * nv_is_rw
+            (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX])
+                * (P::ONES - nv[COL_MEM_ENV_IDX] + lv[COL_MEM_ENV_IDX])
                 * (nv_region_heap - region_heap - P::ONES)
+                * (nv_addr - addr - nv_diff_addr),
+        );
+        // constaint rw_addr_unchanged, for stack and heap
+        yield_constr.constraint_transition(
+            (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX])
+                * (P::ONES - nv[COL_MEM_ENV_IDX] + lv[COL_MEM_ENV_IDX])
+                * region_stack
+                * nv_region_stack
                 * (P::ONES - nv_rw_addr_unchanged - nv_diff_addr * nv_diff_addr_inv),
         );
-
-        yield_constr.constraint(is_rw * (P::ONES - is_rw));
-        yield_constr.constraint(region_prophet * (P::ONES - region_prophet));
-        yield_constr.constraint(region_heap * (P::ONES - region_heap));
-        yield_constr.constraint(region_prophet * (p - addr - diff_addr_cond));
-        yield_constr.constraint(region_heap * (p - span - addr - diff_addr_cond));
+        yield_constr.constraint_transition(
+            (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX])
+                * (P::ONES - nv[COL_MEM_ENV_IDX] + lv[COL_MEM_ENV_IDX])
+                * region_heap
+                * nv_region_heap
+                * (P::ONES - nv_rw_addr_unchanged - nv_diff_addr * nv_diff_addr_inv),
+        );
 
         // for write once:
         // 1. addr doesn't change or increase by 1 in prophet region;
         // 2. when addr not increase, must be read.
-        yield_constr
-            .constraint_transition(region_prophet * (nv_addr - addr) * (nv_addr - addr - P::ONES));
-        yield_constr
-            .constraint_transition(region_prophet * (nv_addr - addr - P::ONES) * nv_is_write);
+        yield_constr.constraint(
+            region_prophet * nv_region_prophet * (nv_addr - addr) * (nv_addr - addr - P::ONES),
+        );
+        yield_constr.constraint(
+            region_prophet * nv_region_prophet * (nv_addr - addr - P::ONES) * nv_is_write,
+        );
+
+        // (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX])
+        // * (nv[COL_MEM_ENV_IDX] - lv[COL_MEM_ENV_IDX]
 
         // read/write constraint:
         // 1. first operation for each addr must be write;
         // 2. next value does not change if it is read.
         yield_constr.constraint_first_row(P::ONES - is_write);
-        yield_constr.constraint_transition((nv_addr - addr) * (P::ONES - nv_is_write));
-        yield_constr.constraint_transition((P::ONES - nv_is_write) * (nv_value - value));
+        yield_constr.constraint(
+            (nv[COL_MEM_TX_IDX] - lv[COL_MEM_TX_IDX])
+                * (nv[COL_MEM_ENV_IDX] - lv[COL_MEM_ENV_IDX])
+                * (P::ONES - nv_is_write),
+        );
+        yield_constr.constraint((nv_addr - addr) * (P::ONES - nv_is_write));
+        yield_constr.constraint((P::ONES - nv_is_write) * (nv_value - value));
 
         // rc_value constraint:
         yield_constr.constraint_transition(
-            is_rw
+            (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX])
+                * (P::ONES - nv[COL_MEM_ENV_IDX] + lv[COL_MEM_ENV_IDX])
+                * is_rw
                 * (nv_region_heap - region_heap - P::ONES)
                 * (rc_value - rw_addr_unchanged * diff_clk)
                 * (rc_value - (P::ONES - rw_addr_unchanged) * diff_addr),
         );
         yield_constr.constraint_transition(
-            is_rw
+            (P::ONES - nv[COL_MEM_TX_IDX] + lv[COL_MEM_TX_IDX])
+                * (P::ONES - nv[COL_MEM_ENV_IDX] + lv[COL_MEM_ENV_IDX])
+                * is_rw
                 * rc_value
                 * (nv_region_heap - region_heap - P::ONES)
                 * (P::ONES - filter_looking_rc),
@@ -175,7 +260,7 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for MemoryStark<F
     }
 
     fn constraint_degree(&self) -> usize {
-        6
+        9
     }
 }
 
