f4b260d30de9453e3f2bf22cce0c8dffdd5c7f79
Softcloud88 <softcloud1988@gmail.com>
2023-09-04 19:30:23 +0800

    MOD: MemoryStark for tape and sccall.

--- circuits/src/memory/columns.rs
+++ circuits/src/memory/columns.rs
@@ -9,11 +9,21 @@ use std::collections::BTreeMap;
 // │ filter_looked_for_main │ rw_addr_unchanged │ region_prophet │
 // region_poseidon │ region_ecdsa │ rc_value │ filter_looking_rc │
 // ┴────────────────────────┴───────────────────┴────────────────┴─────────────────┴──────────────┴──────────┴───────────────────┘
-pub(crate) const COL_MEM_IS_RW: usize = 0;
+pub(crate) const COL_MEM_TX_IDX: usize = 0;
+pub(crate) const COL_MEM_ENV_IDX: usize = COL_MEM_TX_IDX + 1;
+pub(crate) const COL_MEM_IS_RW: usize = COL_MEM_ENV_IDX + 1;
 pub(crate) const COL_MEM_ADDR: usize = COL_MEM_IS_RW + 1;
 pub(crate) const COL_MEM_CLK: usize = COL_MEM_ADDR + 1;
 pub(crate) const COL_MEM_OP: usize = COL_MEM_CLK + 1;
-pub(crate) const COL_MEM_IS_WRITE: usize = COL_MEM_OP + 1;
+pub(crate) const COL_MEM_S_MLOAD : usize= COL_MEM_OP + 1;
+pub(crate) const COL_MEM_S_MSTORE : usize= COL_MEM_S_MLOAD + 1;
+pub(crate) const COL_MEM_S_CALL : usize= COL_MEM_S_MSTORE + 1;
+pub(crate) const COL_MEM_S_RET : usize= COL_MEM_S_CALL + 1;
+pub(crate) const COL_MEM_S_TLOAD : usize= COL_MEM_S_RET + 1;
+pub(crate) const COL_MEM_S_TSTORE : usize= COL_MEM_S_TLOAD + 1;
+pub(crate) const COL_MEM_S_SCCALL : usize= COL_MEM_S_TSTORE + 1;
+pub(crate) const COL_MEM_S_PROPHET : usize= COL_MEM_S_SCCALL + 1;
+pub(crate) const COL_MEM_IS_WRITE: usize = COL_MEM_S_PROPHET + 1;
 pub(crate) const COL_MEM_VALUE: usize = COL_MEM_IS_WRITE + 1;
 pub(crate) const COL_MEM_DIFF_ADDR: usize = COL_MEM_VALUE + 1;
 pub(crate) const COL_MEM_DIFF_ADDR_INV: usize = COL_MEM_DIFF_ADDR + 1;
@@ -30,10 +40,20 @@ pub(crate) const NUM_MEM_COLS: usize = COL_MEM_FILTER_LOOKING_RC_COND + 1;
 
 pub(crate) fn get_memory_col_name_map() -> BTreeMap<usize, String> {
     let mut m: BTreeMap<usize, String> = BTreeMap::new();
+    m.insert(COL_MEM_TX_IDX, String::from("TX_IDX"));
+    m.insert(COL_MEM_ENV_IDX, String::from("ENV_IDX"));
     m.insert(COL_MEM_IS_RW, String::from("IS_RW"));
     m.insert(COL_MEM_ADDR, String::from("ADDR"));
     m.insert(COL_MEM_CLK, String::from("CLK"));
     m.insert(COL_MEM_OP, String::from("OP"));
+    m.insert(COL_MEM_S_MLOAD, String::from("S_MLOAD"));
+    m.insert(COL_MEM_S_MSTORE, String::from("S_MSTORE"));
+    m.insert(COL_MEM_S_CALL, String::from("S_CALL"));
+    m.insert(COL_MEM_S_RET, String::from("S_RET"));
+    m.insert(COL_MEM_S_TLOAD, String::from("S_TLOAD"));
+    m.insert(COL_MEM_S_TSTORE, String::from("S_TSTORE"));
+    m.insert(COL_MEM_S_SCCALL, String::from("S_SCCALL"));
+    m.insert(COL_MEM_S_PROPHET, String::from("S_PROPHET"));
     m.insert(COL_MEM_IS_WRITE, String::from("IS_WRITE"));
     m.insert(COL_MEM_VALUE, String::from("VALUE"));
     m.insert(COL_MEM_DIFF_ADDR, String::from("DIFF_ADDR"));
