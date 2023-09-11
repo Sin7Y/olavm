2f1c43816ed3c77b37acb24bf831d5e071b6974f
peinlcy <peinlcy@gmail.com>
2023-09-11 19:07:58 +0800

    TMP: add example

--- plonky2/field/src/cfft/mod.rs
+++ plonky2/field/src/cfft/mod.rs
@@ -22,7 +22,7 @@ const MIN_CONCURRENT_SIZE: usize = 1024;
 
 #[cfg(feature = "cuda")]
 lazy_static! {
-    static ref CUDA_SP: Arc<Semaphore> = Arc::new(Semaphore::new(2));
+    static ref CUDA_SP: Arc<Semaphore> = Arc::new(Semaphore::new(1));
     // static ref RT: Runtime = Runtime::new().unwrap();
 }
 
@@ -51,21 +51,15 @@ where
     // function; unless the polynomial is small, then don't bother with the
     // concurrent version
     if cfg!(feature = "cuda") && p[0].as_any().is::<GoldilocksField>() {
-        // #[cfg(feature = "cuda")]
-        // {
-        //     RT.block_on(async {
-        //         let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
-        //         let p2 = run_evaluate_poly(p);
-        //         for (item1, &item2) in p.iter_mut().zip(p2.iter()) {
-        //             *item1 = item2;
-        //         }
-        //         drop(permit);
-        //     });
-        // }
-        let p2 = run_evaluate_poly(p);
-        for (item1, &item2) in p.iter_mut().zip(p2.iter()) {
-            *item1 = item2;
-        }
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
+            let p2 = run_evaluate_poly(p);
+            for (item1, &item2) in p.iter_mut().zip(p2.iter()) {
+                *item1 = item2;
+            }
+            drop(permit);
+        });
     } else {
         if cfg!(feature = "parallel") && p.len() >= MIN_CONCURRENT_SIZE {
             #[cfg(feature = "parallel")]
