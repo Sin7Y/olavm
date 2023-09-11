1efea7fa43a2e0135539b91f795b3ece650a2adf
peinlcy <peinlcy@gmail.com>
2023-09-11 15:06:32 +0800

    TMP: add example

--- plonky2/field/src/cfft/mod.rs
+++ plonky2/field/src/cfft/mod.rs
@@ -52,20 +52,10 @@ where
     // concurrent version
     if cfg!(feature = "cuda") && p[0].as_any().is::<GoldilocksField>() {
         #[cfg(feature = "cuda")]
-        {
-            RT.block_on(async {
-                let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
-                let p2 = run_evaluate_poly(p);
-                for (item1, &item2) in p.iter_mut().zip(p2.iter()) {
-                    *item1 = item2;
-                }
-                drop(permit);
-            });
+        let p2 = run_evaluate_poly(p);
+        for (item1, &item2) in p.iter_mut().zip(p2.iter()) {
+            *item1 = item2;
         }
-        // let p2 = run_evaluate_poly(p);
-        // for (item1, &item2) in p.iter_mut().zip(p2.iter()) {
-        //     *item1 = item2;
-        // }
     } else {
         if cfg!(feature = "parallel") && p.len() >= MIN_CONCURRENT_SIZE {
             #[cfg(feature = "parallel")]
@@ -116,12 +106,7 @@ where
     // concurrent version
     if cfg!(feature = "cuda") && p[0].as_any().is::<GoldilocksField>() {
         #[cfg(feature = "cuda")]
-        RT.block_on(async {
-            let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
-            result = run_evaluate_poly_with_offset(p, domain_offset, blowup_factor);
-            drop(permit);
-        });
-        // result = run_evaluate_poly_with_offset(p, domain_offset, blowup_factor);
+        result = run_evaluate_poly_with_offset(p, domain_offset, blowup_factor);
     } else {
         if cfg!(feature = "parallel") && p.len() >= MIN_CONCURRENT_SIZE {
             #[cfg(feature = "parallel")]
@@ -163,19 +148,15 @@ where
     // interpolate_poly; unless the number of evaluations is small, then don't
     // bother with the concurrent version
     if cfg!(feature = "cuda") && evaluations[0].as_any().is::<GoldilocksField>() {
-        // #[cfg(feature = "cuda")]
-        // RT.block_on(async {
-        //     let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
-        //     let p2 = run_interpolate_poly(evaluations);
-        //     for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
-        //         *item1 = item2;
-        //     }
-        //     drop(permit);
-        // });
-        let p2 = run_interpolate_poly(evaluations);
-        for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
-            *item1 = item2;
-        }
+        #[cfg(feature = "cuda")]
+        RT.block_on(async {
+            let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
+            let p2 = run_interpolate_poly(evaluations);
+            for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
+                *item1 = item2;
+            }
+            drop(permit);
+        });
     } else {
         if cfg!(feature = "parallel") && evaluations.len() >= MIN_CONCURRENT_SIZE {
             #[cfg(feature = "parallel")]
@@ -213,19 +194,15 @@ where
     // function; unless the polynomial is small, then don't bother with the
     // concurrent version
     if cfg!(feature = "cuda") && evaluations[0].as_any().is::<GoldilocksField>() {
-        // #[cfg(feature = "cuda")]
-        // RT.block_on(async {
-        //     let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
-        //     let p2 = run_interpolate_poly_with_offset(evaluations, domain_offset);
-        //     for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
-        //         *item1 = item2;
-        //     }
-        //     drop(permit);
-        // });
-        let p2 = run_interpolate_poly_with_offset(evaluations, domain_offset);
-        for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
-            *item1 = item2;
-        }
+        #[cfg(feature = "cuda")]
+        RT.block_on(async {
+            let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
+            let p2 = run_interpolate_poly_with_offset(evaluations, domain_offset);
+            for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
+                *item1 = item2;
+            }
+            drop(permit);
+        });
     } else {
         if cfg!(feature = "parallel") && evaluations.len() >= MIN_CONCURRENT_SIZE {
             #[cfg(feature = "parallel")]
