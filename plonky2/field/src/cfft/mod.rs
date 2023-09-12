2939925f9ee298b763d85ab1e0555f0fe1b4ba54
peinlcy <peinlcy@gmail.com>
2023-09-12 10:52:32 +0800

    TMP: add example

--- plonky2/field/src/cfft/mod.rs
+++ plonky2/field/src/cfft/mod.rs
@@ -23,7 +23,7 @@ const MIN_CONCURRENT_SIZE: usize = 1024;
 #[cfg(feature = "cuda")]
 lazy_static! {
     static ref CUDA_SP: Arc<Semaphore> = Arc::new(Semaphore::new(1));
-    // static ref RT: Runtime = Runtime::new().unwrap();
+    static ref RT: Runtime = tokio::runtime::Builder::new_current_thread();
 }
 
 pub fn evaluate_poly<F>(p: &mut [F], twiddles: &[F])
