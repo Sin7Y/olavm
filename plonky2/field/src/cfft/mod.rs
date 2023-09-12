2a306d7c1cb8b39dd76bfd61936abe16df1bb156
peinlcy <peinlcy@gmail.com>
2023-09-12 12:08:44 +0800

    TMP: add example

--- plonky2/field/src/cfft/mod.rs
+++ plonky2/field/src/cfft/mod.rs
@@ -53,8 +53,8 @@ where
     // function; unless the polynomial is small, then don't bother with the
     // concurrent version
     if cfg!(feature = "cuda") && p[0].as_any().is::<GoldilocksField>() {
-        // let rt = Runtime::new().unwrap();
-        RT.block_on(async {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
             let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
             let p2 = run_evaluate_poly(p);
             for (item1, &item2) in p.iter_mut().zip(p2.iter()) {
@@ -111,8 +111,8 @@ where
     // function; unless the polynomial is small, then don't bother with the
     // concurrent version
     if cfg!(feature = "cuda") && p[0].as_any().is::<GoldilocksField>() {
-        // let rt = Runtime::new().unwrap();
-        RT.block_on(async {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
             let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
             result = run_evaluate_poly_with_offset(p, domain_offset, blowup_factor);
             drop(permit);
@@ -158,8 +158,8 @@ where
     // interpolate_poly; unless the number of evaluations is small, then don't
     // bother with the concurrent version
     if cfg!(feature = "cuda") && evaluations[0].as_any().is::<GoldilocksField>() {
-        // let rt = Runtime::new().unwrap();
-        RT.block_on(async {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
             let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
             let p2 = run_interpolate_poly(evaluations);
             for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
