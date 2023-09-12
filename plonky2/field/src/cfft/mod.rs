b552d8a3be229c89c6d44a3815b4e8a32fd7e523
peinlcy <peinlcy@gmail.com>
2023-09-12 15:21:16 +0800

    TMP: add example

--- plonky2/field/src/cfft/mod.rs
+++ plonky2/field/src/cfft/mod.rs
@@ -204,8 +204,8 @@ where
     // function; unless the polynomial is small, then don't bother with the
     // concurrent version
     if cfg!(feature = "cuda") && evaluations[0].as_any().is::<GoldilocksField>() {
-        let rt = build_runtime();
-        rt.block_on(async {
+        // let rt = build_runtime();
+        RT.block_on(async {
             let permit = CUDA_SP.clone().acquire_owned().await.unwrap();
             let p2 = run_interpolate_poly_with_offset(evaluations, domain_offset);
             for (item1, &item2) in evaluations.iter_mut().zip(p2.iter()) {
