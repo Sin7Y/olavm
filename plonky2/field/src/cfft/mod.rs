3f33710817de28183c4b6756910cd1982a0a6743
peinlcy <peinlcy@gmail.com>
2023-09-13 16:42:46 +0800

    TMP: add example

--- plonky2/field/src/cfft/mod.rs
+++ plonky2/field/src/cfft/mod.rs
@@ -139,9 +139,9 @@ where
     if cfg!(feature = "cuda") && p[0].as_any().is::<GoldilocksField>() {
         let gpu = Arc::clone(&GPU_LOCK);
         let mut gpu = gpu.lock().unwrap();
-        // result = run_evaluate_poly_with_offset(p, domain_offset, blowup_factor);
+        result = run_evaluate_poly_with_offset(p, domain_offset, blowup_factor);
 
-        result = serial::evaluate_poly_with_offset(p, twiddles, domain_offset, blowup_factor);
+        // result = serial::evaluate_poly_with_offset(p, twiddles, domain_offset, blowup_factor);
 
         *gpu += 1;
         // let rt = build_runtime();
