2a484fb4123e9039db056022c6b61fb05a306b6e
Softcloud88 <softcloud1988@gmail.com>
2023-11-08 22:08:39 +0800

    MOD: bench.

--- executor/src/tests.rs
+++ executor/src/tests.rs
@@ -176,7 +176,7 @@ fn fibo_recursive() {
 
 #[test]
 fn prophet_sqrt_test() {
-    let calldata = [144u64, 3000u64, 2u64, 3509365327u64]
+    let calldata = [1073741824u64, 7000u64, 2u64, 3509365327u64]
         .iter()
         .map(|v| GoldilocksField::from_canonical_u64(*v))
         .collect_vec();
