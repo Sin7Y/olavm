91acd10699b24c19486a40d439164a9fec4e5fb2
Bob Niu <spartucus@users.noreply.github.com>
2023-12-01 16:35:40 +0800

    Fix missing directory issue (#172)

--- assembler/src/test_data_generator.rs
+++ assembler/src/test_data_generator.rs
@@ -200,7 +200,7 @@ mod tests {
         generate_from_file("ptr_call.json".to_string(), "ptr_call.json".to_string());
     }
     fn generate_from_file(input_file_name: String, output_file_name: String) {
-        let _ = fs::create_dir_all("test_data/bin");
+        let _ = fs::create_dir_all("test_data/bin/sccall");
         let input_path = format!("test_data/asm/{}", input_file_name);
         let json_str = std::fs::read_to_string(&input_path).unwrap();
         let bundle: AsmBundle = serde_json::from_str(json_str.as_str()).unwrap();
