169f14bfbbdd8aa3c1462b4ec338821e6947d571
yp945 <pan107104@outlook.com>
2023-12-25 17:26:16 +0800

    update signature context data.

--- assembler/src/test_data_generator.rs
+++ assembler/src/test_data_generator.rs
@@ -289,9 +289,6 @@ mod tests {
         generate_from_file("system_context_asm.json".to_string(), "system_context.json".to_string());
     }
 
-
-
-
     fn generate_from_file(input_file_name: String, output_file_name: String) {
         let _ = fs::create_dir_all("test_data/bin/sccall");
         let input_path = format!("test_data/asm/{}", input_file_name);
