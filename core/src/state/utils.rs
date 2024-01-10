84f7005f0d0114b9420c7265650a0906369f1120
web3Softcloud <renzhexin@web3.com>
2024-01-10 17:39:40 +0800

    MOD: fetch prog hash.

--- core/src/state/utils.rs
+++ core/src/state/utils.rs
@@ -1,4 +1,3 @@
-use blake2::{Blake2s256, Digest};
 use plonky2::{field::types::PrimeField64, hash::utils::poseidon_hash_bytes};
 use web3::types::{H256, U256};
 
@@ -11,17 +10,17 @@ pub fn get_prog_hash_cf_key_from_contract_addr(contract_addr: &TreeKey) -> [u8; 
         0x80, 0x02,
     ]);
 
-    let account: [u8; 32] = contract_addr
+    let slot: [u8; 32] = contract_addr
         .iter()
         .flat_map(|&field| field.to_canonical_u64().to_be_bytes().to_vec())
         .collect::<Vec<u8>>()
         .try_into()
         .unwrap();
-    let key = [[0; 32], account].concat();
+    let key = [[0; 32], slot].concat();
     let key = H256(poseidon_hash_bytes(&key));
 
     let mut bytes = [0_u8; 64];
     bytes[0..32].copy_from_slice(&address.0);
     U256::from(key.to_fixed_bytes()).to_big_endian(&mut bytes[32..64]);
-    Blake2s256::digest(bytes).into()
+    poseidon_hash_bytes(&bytes)
 }
