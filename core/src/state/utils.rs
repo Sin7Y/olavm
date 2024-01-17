c40d6086259732a154579c169e483d2527312a06
Bob Niu <spartucus@users.noreply.github.com>
2024-01-17 19:32:36 +0800

    error handling progressing

--- core/src/state/utils.rs
+++ core/src/state/utils.rs
@@ -3,7 +3,9 @@ use web3::types::{H256, U256};
 
 use crate::types::merkle_tree::TreeKey;
 
-pub fn get_prog_hash_cf_key_from_contract_addr(contract_addr: &TreeKey) -> [u8; 32] {
+pub fn get_prog_hash_cf_key_from_contract_addr(
+    contract_addr: &TreeKey,
+) -> Result<[u8; 32], String> {
     let address = H256([
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -15,12 +17,12 @@ pub fn get_prog_hash_cf_key_from_contract_addr(contract_addr: &TreeKey) -> [u8; 
         .flat_map(|&field| field.to_canonical_u64().to_be_bytes().to_vec())
         .collect::<Vec<u8>>()
         .try_into()
-        .unwrap();
+        .map_err(|err| format!("Vec<u8> {:?} convert to [u8;32] failed", err))?;
     let key = [[0; 32], slot].concat();
     let key = H256(poseidon_hash_bytes(&key));
 
     let mut bytes = [0_u8; 64];
     bytes[0..32].copy_from_slice(&address.0);
     U256::from(key.to_fixed_bytes()).to_big_endian(&mut bytes[32..64]);
-    poseidon_hash_bytes(&bytes)
+    Ok(poseidon_hash_bytes(&bytes))
 }
