fd9c53f7a9ce0709568658d541159b6ff124ac0b
Softcloud <Softcloud1988@gmail.com>
2024-01-26 17:01:14 +0800

    ADD: init mini-ola.

---
+++ mini-ola/src/main.rs
@@ -0,0 +1,41 @@
+use clap::{CommandFactory, Parser, Subcommand};
+use colored::Colorize;
+use subcommands::deploy::Deploy;
+
+mod path;
+mod subcommands;
+
+#[derive(Debug, Parser)]
+#[clap(author, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Subcommands>,
+    #[clap(long = "version", short = 'V', help = "Print version info and exit")]
+    version: bool,
+}
+
+#[derive(Debug, Subcommand)]
+enum Subcommands {
+    #[clap(about = "Deploy a ola contract locally.")]
+    Deploy(Deploy),
+}
+
+fn main() {
+    if let Err(err) = run_command(Cli::parse()) {
+        eprintln!("{}", format!("Error: {err}").red());
+        std::process::exit(1);
+    }
+}
+
+fn run_command(cli: Cli) -> anyhow::Result<()> {
+    match (cli.version, cli.command) {
+        (false, None) => Ok(Cli::command().print_help()?),
+        (true, _) => {
+            println!("{}", env!("CARGO_PKG_VERSION"));
+            Ok(())
+        }
+        (false, Some(command)) => match command {
+            Subcommands::Deploy(cmd) => cmd.run(),
+        },
+    }
+}
