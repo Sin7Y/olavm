48d9bb58bcb71e48aabb4a239f1df90852ce02c3
Softcloud <Softcloud1988@gmail.com>
2024-02-14 10:23:27 +0800

    ADD: assert process.

--- executor/src/contract_executor.rs
+++ executor/src/contract_executor.rs
@@ -12,7 +12,7 @@ use core::{
         },
         opcodes::OlaOpcode,
         operands::OlaOperand,
-        vm_state::{OlaStateDiff, RegisterDiff},
+        vm_state::{OlaStateDiff, RegisterDiff, SpecRegisterDiff},
     },
 };
 
@@ -23,7 +23,7 @@ use crate::{config::ExecuteMode, ola_storage::OlaCachedStorage};
 pub(crate) struct OlaContractExecutor<'a> {
     mode: ExecuteMode,
     context: ExeContext,
-    clk: u32,
+    clk: u64,
     pc: u64,
     psp: u64,
     registers: [u64; NUM_GENERAL_PURPOSE_REGISTER],
@@ -82,7 +82,7 @@ impl<'a> OlaContractExecutor<'a> {
             | OlaOpcode::NEQ
             | OlaOpcode::GTE => self.process_two_operands_arithmetic_op(instruction),
 
-            OlaOpcode::ASSERT => todo!(),
+            OlaOpcode::ASSERT => self.process_assert(instruction),
             OlaOpcode::MOV => todo!(),
             OlaOpcode::JMP => todo!(),
             OlaOpcode::CJMP => todo!(),
@@ -109,6 +109,7 @@ impl<'a> OlaContractExecutor<'a> {
         &mut self,
         instruction: BinaryInstruction,
     ) -> anyhow::Result<(Vec<OlaStateDiff>, Option<ExeTraceStepDiff>)> {
+        let inst_len = instruction.binary_length();
         let op0 = match instruction.op0 {
             Some(op0) => self.get_operand_value(op0)?,
             None => {
@@ -164,7 +165,7 @@ impl<'a> OlaContractExecutor<'a> {
         }
 
         let cpu_piece = CpuExePiece {
-            clk: self.clk as u64,
+            clk: self.clk,
             pc: self.pc,
             psp: self.psp,
             registers: self.registers,
@@ -218,11 +219,15 @@ impl<'a> OlaContractExecutor<'a> {
             }
         };
 
-        let reg_diff = RegisterDiff {
+        let spec_reg_diff = OlaStateDiff::SpecReg(SpecRegisterDiff {
+            pc: Some(self.pc + inst_len as u64),
+            psp: None,
+        });
+        let reg_diff = OlaStateDiff::Register(vec![RegisterDiff {
             register: dst_reg,
             value: res,
-        };
-        let state_diff = OlaStateDiff::Register(vec![reg_diff]);
+        }]);
+        let state_diff = vec![spec_reg_diff, reg_diff];
         let trace_diff = if self.is_trace_needed() {
             Some(ExeTraceStepDiff {
                 pc: self.pc,
@@ -238,7 +243,77 @@ impl<'a> OlaContractExecutor<'a> {
         } else {
             None
         };
-        Ok((vec![state_diff], trace_diff))
+        Ok((state_diff, trace_diff))
+    }
+
+    fn process_assert(
+        &self,
+        instruction: BinaryInstruction,
+    ) -> anyhow::Result<(Vec<OlaStateDiff>, Option<ExeTraceStepDiff>)> {
+        let inst_len = instruction.binary_length();
+        let op0 = match instruction.op0 {
+            Some(op0) => self.get_operand_value(op0)?,
+            None => {
+                return Err(ProcessorError::InvalidInstruction(format!(
+                    "op0 must set for {}",
+                    instruction.opcode.to_string()
+                ))
+                .into())
+            }
+        };
+        let dst_reg = match instruction.dst {
+            Some(dst) => match dst {
+                OlaOperand::RegisterOperand { register } => register,
+                _ => {
+                    return Err(ProcessorError::InvalidInstruction(format!(
+                        "dst must be a register for {}",
+                        instruction.opcode.to_string()
+                    ))
+                    .into())
+                }
+            },
+            None => {
+                return Err(ProcessorError::InvalidInstruction(format!(
+                    "dst must set for {}",
+                    instruction.opcode.to_string()
+                ))
+                .into())
+            }
+        };
+        if op0 != 1 {
+            return Err(ProcessorError::AssertFail(dst_reg.index() as u64, op0).into());
+        }
+
+        let spec_reg_diff = OlaStateDiff::SpecReg(SpecRegisterDiff {
+            pc: Some(self.pc + inst_len as u64),
+            psp: None,
+        });
+        let state_diff = vec![spec_reg_diff];
+        let trace_diff = if self.is_trace_needed() {
+            Some(ExeTraceStepDiff {
+                pc: self.pc,
+                cpu: Some(CpuExePiece {
+                    clk: self.clk,
+                    pc: self.pc,
+                    psp: self.psp,
+                    registers: self.registers,
+                    opcode: self.instructions[self.pc as usize].opcode,
+                    op0: Some(op0),
+                    op1: None,
+                    dst: None,
+                }),
+                mem: None,
+                rc: None,
+                bitwise: None,
+                cmp: None,
+                poseidon: None,
+                tape: None,
+                storage: None,
+            })
+        } else {
+            None
+        };
+        Ok((state_diff, trace_diff))
     }
 
     fn get_operand_value(&self, operand: OlaOperand) -> anyhow::Result<u64> {
