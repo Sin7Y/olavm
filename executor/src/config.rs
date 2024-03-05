0cdfd868a92004170b398c2ceba07258c8a2fd5a
Softcloud <Softcloud1988@gmail.com>
2024-03-05 23:00:54 +0800

    ADD: test for deploy.

--- executor/src/config.rs
+++ executor/src/config.rs
@@ -1,3 +1,13 @@
+use core::vm::hardware::ContractAddress;
+
+pub const ADDR_U64_ENTRYPOINT: ContractAddress = [0, 0, 0, 32769];
+pub const ADDR_U64_CODE_STORAGE: ContractAddress = [0, 0, 0, 32770];
+pub const ADDR_U64_NONCE_HOLDER: ContractAddress = [0, 0, 0, 32771];
+pub const ADDR_U64_KNOWN_CODES_STORAGE: ContractAddress = [0, 0, 0, 32772];
+pub const ADDR_U64_CONTRACT_DEPLOYER: ContractAddress = [0, 0, 0, 32773];
+pub const ADDR_U64_DEFAULT_ACCOUNT: ContractAddress = [0, 0, 0, 32774];
+pub const ADDR_U64_SYSTEM_CONTEXT: ContractAddress = [0, 0, 0, 32779];
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub(crate) enum ExecuteMode {
     Invoke,
