26e1c73d3a433a0b559ae6d66dd9302f8fe3485a
Softcloud <Softcloud1988@gmail.com>
2024-03-19 22:08:24 +0800

    ADD: finish batch.

--- core/src/tx/mod.rs
+++ core/src/tx/mod.rs
@@ -9,3 +9,10 @@ pub struct TxResult {
     pub storage_access_logs: Vec<StorageAccessLog>,
     pub events: Vec<Event>,
 }
+
+pub struct BatchResult {
+    pub tx_traces: Vec<TxExeTrace>,
+    pub storage_access_logs: Vec<StorageAccessLog>,
+    pub events: Vec<Event>,
+    pub block_tip_queries: Vec<StorageAccessLog>,
+}
