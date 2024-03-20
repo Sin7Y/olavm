37f16f5f726f6a309a7c16a7f60f217438866d8f
Softcloud <Softcloud1988@gmail.com>
2024-03-20 16:37:34 +0800

    ADD: entrance contract address parse from entry point.

--- executor/src/config.rs
+++ executor/src/config.rs
@@ -8,6 +8,8 @@ pub const ADDR_U64_CONTRACT_DEPLOYER: ContractAddress = [0, 0, 0, 32773];
 pub const ADDR_U64_DEFAULT_ACCOUNT: ContractAddress = [0, 0, 0, 32774];
 pub const ADDR_U64_SYSTEM_CONTEXT: ContractAddress = [0, 0, 0, 32779];
 
+pub const FUNCTION_SELECTOR_SYSTEM_ENTRANCE: u64 = 3234502684;
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum ExecuteMode {
     Invoke,
