c15a066fca32c377fe1f413802bae481dc31c9bc
Softcloud <Softcloud1988@gmail.com>
2024-03-30 16:43:06 +0800

    ADD: gen trace.

--- circuits/src/generation/poseidon.rs
+++ circuits/src/generation/poseidon.rs
@@ -137,3 +137,141 @@ pub fn generate_poseidon_trace<F: RichField>(cells: &[PoseidonRow]) -> [Vec<F>; 
         )
     })
 }
+
+pub fn gen_poseidon_trace<F: RichField>(
+    batch_cells: &Vec<Vec<PoseidonRow>>,
+) -> [Vec<F>; NUM_POSEIDON_COLS] {
+    let num_filled_row_len: usize = batch_cells.iter().map(|inner_vec| inner_vec.len()).sum();
+    let num_padded_rows = if !num_filled_row_len.is_power_of_two() || num_filled_row_len < 2 {
+        if num_filled_row_len < 2 {
+            2
+        } else {
+            num_filled_row_len.next_power_of_two()
+        }
+    } else {
+        num_filled_row_len
+    };
+
+    let mut trace: Vec<Vec<F>> = vec![vec![F::ZERO; num_padded_rows]; NUM_POSEIDON_COLS];
+    let mut i = 0;
+    for tx_steps in batch_cells.iter() {
+        for c in tx_steps.iter() {
+            trace[FILTER_LOOKED_NORMAL][i] = if c.filter_looked_normal {
+                F::ONE
+            } else {
+                F::ZERO
+            };
+            trace[FILTER_LOOKED_TREEKEY][i] = if c.filter_looked_treekey {
+                F::ONE
+            } else {
+                F::ZERO
+            };
+            trace[FILTER_LOOKED_STORAGE_LEAF][i] = if c.filter_looked_storage {
+                F::ONE
+            } else {
+                F::ZERO
+            };
+            trace[FILTER_LOOKED_STORAGE_BRANCH][i] = if c.filter_looked_storage_branch {
+                F::ONE
+            } else {
+                F::ZERO
+            };
+
+            for j in 0..12 {
+                trace[COL_POSEIDON_INPUT_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.input[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_OUTPUT_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.output[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_0_1_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.full_0_1[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_0_2_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.full_0_2[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_0_3_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.full_0_3[j].to_canonical_u64());
+            }
+            for j in 0..22 {
+                trace[COL_POSEIDON_PARTIAL_ROUND_ELEMENT_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.partial[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_0_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.full_1_0[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_1_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.full_1_1[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_2_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.full_1_2[j].to_canonical_u64());
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_3_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(c.full_1_3[j].to_canonical_u64());
+            }
+            i += 1;
+        }
+    }
+
+    // Pad trace to power of two.
+    if num_padded_rows != num_filled_row_len {
+        for i in num_filled_row_len..num_padded_rows {
+            for j in 0..12 {
+                trace[COL_POSEIDON_INPUT_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_INPUT[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_OUTPUT_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_OUTPUT[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_0_1_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_FULL_0_1[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_0_2_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_FULL_0_2[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_0_3_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_FULL_0_3[j]);
+            }
+            for j in 0..22 {
+                trace[COL_POSEIDON_PARTIAL_ROUND_ELEMENT_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_PARTIAL[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_0_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_FULL_1_0[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_1_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_FULL_1_1[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_2_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_FULL_1_2[j]);
+            }
+            for j in 0..12 {
+                trace[COL_POSEIDON_FULL_ROUND_1_3_STATE_RANGE.start + j][i] =
+                    F::from_canonical_u64(POSEIDON_ZERO_HASH_FULL_1_3[j]);
+            }
+        }
+    }
+
+    trace.try_into().unwrap_or_else(|v: Vec<Vec<F>>| {
+        panic!(
+            "Expected a Vec of length {} but it was {}",
+            NUM_POSEIDON_COLS,
+            v.len()
+        )
+    })
+}
--- circuits/src/generation/tape.rs
+++ circuits/src/generation/tape.rs
@@ -71,3 +71,71 @@ pub fn generate_tape_trace<F: RichField>(cells: &[TapeRow]) -> [Vec<F>; NUM_COL_
         )
     })
 }
+
+pub fn gen_tape_trace<F: RichField>(batch_cells: &Vec<Vec<TapeRow>>) -> [Vec<F>; NUM_COL_TAPE] {
+    let num_filled_row_len: usize = batch_cells.iter().map(|inner_vec| inner_vec.len()).sum();
+    let num_padded_rows = if !num_filled_row_len.is_power_of_two() || num_filled_row_len < 2 {
+        if num_filled_row_len < 2 {
+            2
+        } else {
+            num_filled_row_len.next_power_of_two()
+        }
+    } else {
+        num_filled_row_len
+    };
+
+    let mut trace: Vec<Vec<F>> = vec![vec![F::ZERO; num_padded_rows]; NUM_COL_TAPE];
+    let mut i = 0;
+    for (tx_idx, tx_steps) in batch_cells.iter().enumerate() {
+        for c in tx_steps.iter() {
+            trace[COL_TAPE_TX_IDX][i] = F::from_canonical_usize(tx_idx);
+            trace[COL_TAPE_IS_INIT_SEG][i] = if c.is_init { F::ONE } else { F::ZERO };
+            trace[COL_TAPE_OPCODE][i] = F::from_canonical_u64(c.opcode.to_canonical_u64());
+            trace[COL_TAPE_ADDR][i] = F::from_canonical_u64(c.addr.to_canonical_u64());
+            trace[COL_TAPE_VALUE][i] = F::from_canonical_u64(c.value.to_canonical_u64());
+            trace[COL_FILTER_LOOKED][i] = F::from_canonical_u64(c.filter_looked.to_canonical_u64());
+            i += 1;
+        }
+    }
+    let last_tx_idx = if num_filled_row_len == 0 {
+        F::ZERO
+    } else {
+        trace[COL_TAPE_TX_IDX][num_filled_row_len - 1]
+    };
+    let last_is_init = if num_filled_row_len == 0 {
+        F::ZERO
+    } else {
+        trace[COL_TAPE_IS_INIT_SEG][num_filled_row_len - 1]
+    };
+    let last_addr = if num_filled_row_len == 0 {
+        F::ZERO
+    } else {
+        trace[COL_TAPE_ADDR][num_filled_row_len - 1]
+    };
+    let last_value = if num_filled_row_len == 0 {
+        F::ZERO
+    } else {
+        trace[COL_TAPE_VALUE][num_filled_row_len - 1]
+    };
+
+    let op_tload = F::from_canonical_u64(OlaOpcode::TLOAD.binary_bit_mask());
+
+    if num_padded_rows != num_filled_row_len {
+        for i in num_filled_row_len..num_padded_rows {
+            trace[COL_TAPE_TX_IDX][i] = last_tx_idx;
+            trace[COL_TAPE_IS_INIT_SEG][i] = last_is_init;
+            trace[COL_TAPE_OPCODE][i] = op_tload;
+            trace[COL_TAPE_ADDR][i] = last_addr;
+            trace[COL_TAPE_VALUE][i] = last_value;
+            trace[COL_FILTER_LOOKED][i] = F::ZERO;
+        }
+    }
+
+    trace.try_into().unwrap_or_else(|v: Vec<Vec<F>>| {
+        panic!(
+            "Expected a Vec of length {} but it was {}",
+            NUM_COL_TAPE,
+            v.len()
+        )
+    })
+}
